14c6031150eb78e7787ed85dacd2e8b151e2f20b
Justin Hohing <jnh0627@yahoo.com>
2021-05-19 00:02:06 -0400

    Updated and added the necessary components

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,12 +1,21 @@
 import React from "react";
+import Jumbotron from "../Jumbotron";
 
 function Nav() {
   return (
+    <div>
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        React Reading List
+      <a className="navbar-brand" id="homeLink" href="/">
+        Book React App
       </a>
-    </nav>
+     <ul className="navbar-nav">
+     <li className="nav-item">
+       <a className="nav-link navbar-brand" id="savedLink" href="/bookshelf">Saved Books</a>
+     </li>
+   </ul>
+ </nav>
+ <Jumbotron/>
+ </div>
   );
 }
 
