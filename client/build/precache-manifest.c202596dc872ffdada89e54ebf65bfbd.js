d3bda6cc197f07e4e9bbbf4931f1143b1dc8e14a
Justin Hohing <jnh0627@yahoo.com>
2021-05-20 23:36:16 -0400

    Created build and updated server.js

---
+++ client/build/precache-manifest.c202596dc872ffdada89e54ebf65bfbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec5d3e791040b00503ead50bd2043492",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8612c1c10c4f0376c213",
+    "url": "/static/css/2.2de4618a.chunk.css"
+  },
+  {
+    "revision": "7fecae6e1be75fa90566",
+    "url": "/static/css/main.f828d23f.chunk.css"
+  },
+  {
+    "revision": "8612c1c10c4f0376c213",
+    "url": "/static/js/2.fb725bc5.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.fb725bc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fecae6e1be75fa90566",
+    "url": "/static/js/main.e42246e1.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
